11708976ab63e1fd6a3960a2e39153b4c87cf6fe
JervyShi <jervyshi@sliendeMacBook-Pro.local>
2014-06-03 23:12:22 +0800

    upload list

--- web/app.py
+++ web/app.py
@@ -1,22 +1,27 @@
 __author__ = 'jervyshi'
 import os
 
+import fileUtil
+
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify
 
 ALLOWED_EXTENSIONS = set(['zip', 'pdf'])
+UPLOAD_FOLDER = '/tmp/verify/'
+WORK_FOLDER = '/tmp/verify_work/'
 
 app = Flask(__name__)
-app.config.from_envvar('APP_SETTINGS', silent=True)
-
+#app.config.from_envvar('APP_SETTINGS', silent=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     # print app.config['UPLOAD_FOLDER']
-    return render_template('index.html')
+    f = fileUtil.get_all_file(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', files=f)
 
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
 
     pass
